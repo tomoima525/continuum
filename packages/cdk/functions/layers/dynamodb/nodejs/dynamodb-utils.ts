16eea7aa91e58612aac579636daa3e4658f59db6
Tomoaki Imai <tomoima525@gmail.com>
2022-04-20 15:25:21 +0200

    Sort by createdAt to generate merkleproof properly

--- packages/cdk/functions/layers/dynamodb/nodejs/dynamodb-utils.ts
+++ packages/cdk/functions/layers/dynamodb/nodejs/dynamodb-utils.ts
@@ -2,6 +2,7 @@ import { nodeGetByHash } from './nodeGet';
 import { nodePut } from './nodePut';
 import { nodeQuery } from './nodeQuery';
 import { nodesCount } from './nodesCount';
+import { nodesQuery } from './nodesQuery';
 import { nodesQueryByGroup } from './nodesByGroup';
 import { createNodePut, createNodeUpdate, nodesTxWrite } from './nodesTxWrite';
 export {
@@ -9,6 +10,7 @@ export {
   nodePut,
   nodeQuery,
   nodesCount,
+  nodesQuery,
   nodesQueryByGroup,
   createNodePut,
   createNodeUpdate,
