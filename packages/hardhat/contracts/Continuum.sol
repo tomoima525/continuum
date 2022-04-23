c127b5bc22b273819cfb8a959cceaf411bfc060e
Tomoaki Imai <tomoima525@gmail.com>
2022-04-23 20:02:42 +0200

    Contract

---
+++ packages/hardhat/contracts/Continuum.sol
@@ -0,0 +1,56 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "./IContinuum.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@appliedzkp/semaphore-contracts/interfaces/IVerifier.sol";
+import "@appliedzkp/semaphore-contracts/base/SemaphoreCore.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+/// @title Continuum
+contract Continuum is IContinuum, SemaphoreCore, ERC721 {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private supplyCounter;
+    IVerifier private verifier;
+
+    /// @dev
+    /// @param verifierAddress: Verifier address.
+    constructor(address verifierAddress) ERC721("Continuum", "CNT") {
+        verifier = IVerifier(verifierAddress);
+    }
+
+    /// @dev See {IContinuum-mint}.
+    function mint(
+        uint256 root,
+        uint256 nullifierHash,
+        uint256 externalNullifier,
+        uint256[8] calldata proof
+    ) external override {
+        bytes32 signal = bytes32("continuum");
+        _verifyProof(
+            signal,
+            root,
+            nullifierHash,
+            externalNullifier,
+            proof,
+            verifier
+        );
+
+        // Prevent double-signaling (nullifierHash = hash(groupId + identityNullifier)).
+        _saveNullifierHash(nullifierHash);
+
+        emit ProofVerified(signal);
+
+        _mint(msg.sender, totalSupply());
+
+        emit Minted(totalSupply(), msg.sender);
+
+        supplyCounter.increment();
+    }
+
+    function totalSupply() public view returns (uint256) {
+        return supplyCounter.current();
+    }
+}
---
+++ packages/hardhat/contracts/IContinuum.sol
@@ -0,0 +1,28 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+/// @title Continuum interface.
+/// @dev Interface of a Continuum contract.
+interface IContinuum {
+    /// @dev Emitted when a Semaphore proof is verified.
+    /// @param signal: Semaphore signal.
+    event ProofVerified(bytes32 signal);
+
+    /// @dev Emitted when a Semaphore proof is verified.
+    /// @param tokenId: Id of the token.
+    /// @param to: address minted
+    event Minted(uint256 tokenId, address to);
+
+    /// @dev Saves the nullifier hash to avoid double signaling and exit an event
+    /// if the zero-knowledge proof is valid.
+    /// @param root: root of tree
+    /// @param nullifierHash: Nullifier hash.
+    /// @param externalNullifier: External nullifier.
+    /// @param proof: Zero-knowledge proof.
+    function mint(
+        uint256 root,
+        uint256 nullifierHash,
+        uint256 externalNullifier,
+        uint256[8] calldata proof
+    ) external;
+}
