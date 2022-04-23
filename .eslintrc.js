f4c5c965013d15117543a241e9d68934e8ca7171
Tomoaki Imai <tomoima525@gmail.com>
2022-04-23 19:56:34 +0200

    TreeSize 16 & import resolver setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,10 +11,20 @@ module.exorts = {
     project: './tsconfig.json',
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ['@typescript-eslint/eslint-plugin', 'import'],
   rules: {
     'no-console': 0,
     semi: 0,
     '@typescript-eslint/no-explicit-any': 0,
+  },
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      typescript: {
+        project: ['packages/configs', 'packages/crypto-tool'],
+      },
+    },
   },
 };
