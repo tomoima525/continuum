5f8dd550736ad97d3baca077b99c7162ba29d746
Tomoaki Imai <tomoima525@gmail.com>
2022-04-24 18:59:59 +0200

    adding was files

--- packages/continuum-web/next.config.js
+++ packages/continuum-web/next.config.js
@@ -2,6 +2,13 @@
 const nextConfig = {
   // fix menu issue https://github.com/tailwindlabs/headlessui/issues/681
   reactStrictMode: false,
+  webpack: function (config, options) {
+    if (!options.isServer) {
+      config.resolve.fallback.fs = false;
+    }
+    config.experiments = { asyncWebAssembly: true, layers: true };
+    return config;
+  },
 };
 
 module.exports = nextConfig;
