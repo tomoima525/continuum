a636f7908a4ba7e4965c3652f0a4a752ad1b732c
Tomoaki Imai <tomoima525@gmail.com>
2022-04-24 19:01:08 +0200

    Network setup for local development

--- packages/continuum-web/src/pages/_app.tsx
+++ packages/continuum-web/src/pages/_app.tsx
@@ -1,30 +1,48 @@
 import '../styles/globals.css';
 import { SessionProvider } from 'next-auth/react';
 import { providers } from 'ethers';
-import { InjectedConnector, Provider, chain } from 'wagmi';
+import {
+  Connector,
+  InjectedConnector,
+  Provider,
+  chain,
+  defaultChains,
+  developmentChains,
+} from 'wagmi';
 import { NotificationProvider } from 'contexts/NotificationContext';
 import type { AppProps } from 'next/app';
-import { Networkish } from '@ethersproject/networks';
 import { Seo } from 'components/ui/Seo';
 import { ContentProvider } from 'contexts/ContentContext';
 
+// Get environment variables
+const alchemyId = process.env.NEXT_PUBLIC_ALCHEMY_ID as string;
+const infuraId = process.env.NEXT_PUBLIC_INFURA_ID as string;
+
 // Chains for connectors to support
-const chains =
-  process.env.NEXT_PUBLIC_BUILD_ENV === 'dev'
-    ? [chain.rinkeby]
-    : [chain.polygonMainnet];
-// const infuraId = process.env.NEXT_PUBLIC_INFURA_KEY;
+// TODO: add harmony network
+const chains = [...defaultChains];
 
-const network: Networkish =
-  process.env.NEXT_PUBLIC_BUILD_ENV === 'dev' ? 'rinkeby' : 'matic';
+// Set up providers
+type ProviderConfig = { chainId?: number; connector?: Connector };
+const isChainSupported = (chainId?: number) => {
+  const supported = chains.some(x => x.id === chainId);
+  console.log(supported, chainId);
+  return supported;
+};
 
-const provider = ({ chainId }: { chainId?: number }) =>
-  new providers.AlchemyProvider(network, process.env.NEXT_PUBLIC_ALCHEMY_KEY);
-
+const provider = ({ chainId }: ProviderConfig) =>
+  providers.getDefaultProvider(
+    isChainSupported(chainId) ? chainId : 'http://localhost:8545',
+    {
+      alchemy: alchemyId,
+      infura: infuraId,
+    },
+  );
 // Set up connectors
 const connectors = ({ chainId }: { chainId?: number }) => {
   return [new InjectedConnector({ chains })];
 };
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider connectors={connectors} provider={provider}>
