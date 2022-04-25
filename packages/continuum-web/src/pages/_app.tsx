a9329f2d127e9f32422a0b587e65cab54eb61ccb
Tomoaki Imai <tomoima525@gmail.com>
2022-04-25 11:47:35 +0200

    Connect Harmony network from the client

--- packages/continuum-web/src/pages/_app.tsx
+++ packages/continuum-web/src/pages/_app.tsx
@@ -1,51 +1,27 @@
 import '../styles/globals.css';
 import { SessionProvider } from 'next-auth/react';
 import { providers } from 'ethers';
-import {
-  Connector,
-  InjectedConnector,
-  Provider,
-  chain,
-  defaultChains,
-  developmentChains,
-} from 'wagmi';
+import { Provider } from 'wagmi';
 import { NotificationProvider } from 'contexts/NotificationContext';
 import type { AppProps } from 'next/app';
 import { Seo } from 'components/ui/Seo';
 import { ContentProvider } from 'contexts/ContentContext';
-
-// Get environment variables
-const alchemyId = process.env.NEXT_PUBLIC_ALCHEMY_ID as string;
-const infuraId = process.env.NEXT_PUBLIC_INFURA_ID as string;
+import networks from '../utils/networks.json';
 
 // Chains for connectors to support
-// TODO: add harmony network
-const chains = [...defaultChains];
+// https://docs.harmony.one/home/developers/network-and-faucets
 
-// Set up providers
-type ProviderConfig = { chainId?: number; connector?: Connector };
-const isChainSupported = (chainId?: number) => {
-  const supported = chains.some(x => x.id === chainId);
-  console.log(supported, chainId);
-  return supported;
-};
-
-const provider = ({ chainId }: ProviderConfig) =>
-  providers.getDefaultProvider(
-    isChainSupported(chainId) ? chainId : 'http://localhost:8545',
-    {
-      alchemy: alchemyId,
-      infura: infuraId,
-    },
-  );
+const provider = providers.getDefaultProvider(
+  networks[networks.selectedChain].rpcUrls[0],
+);
 // Set up connectors
-const connectors = ({ chainId }: { chainId?: number }) => {
-  return [new InjectedConnector({ chains })];
-};
+// const connectors = ({ chainId }: { chainId?: number }) => {
+//   return [new InjectedConnector({ chains })];
+// };
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Provider connectors={connectors} provider={provider}>
+    <Provider autoConnect provider={provider}>
       <SessionProvider session={pageProps.session} refetchInterval={0}>
         <ContentProvider>
           <NotificationProvider>
