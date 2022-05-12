ba9c9b7e360046e7f40c62d6c36ff6c6c2858b0c
Tomoaki Imai <tomoima525@gmail.com>
2022-05-12 22:40:22 +0900

    favicon

--- packages/continuum-web/src/components/ui/Seo.tsx
+++ packages/continuum-web/src/components/ui/Seo.tsx
@@ -44,8 +44,18 @@ export const Seo = ({
         sizes="180x180"
         href="/apple-touch-icon.png"
       />
-      <link rel="icon" type="image/svg" sizes="32x32" href="/favicon.svg" />
-      <link rel="icon" type="image/svg" sizes="16x16" href="/favicon.svg" />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-16x16.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-32x32.png"
+      />
       <link rel="manifest" href="/site.webmanifest" />
       <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#7331FF" />
       <meta name="msapplication-TileColor" content="#7331FF" />
